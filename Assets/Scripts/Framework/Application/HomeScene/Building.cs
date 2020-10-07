639ce2d54a15c49cc561672b57524db7d8e42c4c
tinyshan <tinyshan@tencent.com>
2020-10-07 20:47:00 +0800

    拖拽功能实现

--- Assets/Scripts/Framework/Application/HomeScene/Building.cs
+++ Assets/Scripts/Framework/Application/HomeScene/Building.cs
@@ -7,6 +7,7 @@ public class Building : MonoBehaviour
     , IPointerDownHandler
     , IPointerClickHandler
     , IPointerUpHandler
+    ,IDragHandler
 {
     // Start is called before the first frame update
     void Start()
@@ -21,22 +22,39 @@ public class Building : MonoBehaviour
         
     }
 
+    public void OnDrag(PointerEventData eventData)
+    {
+        // var curPosition = Camera.main.ScreenToWorldPoint(eventData.position);
+        float CosDegreeValue = Mathf.Cos(45 * Mathf.Deg2Rad);
+        float xoffset = (eventData.delta.x) * CosDegreeValue;
+        float zoffset = eventData.delta.x * CosDegreeValue;
+
+        float xxof = -eventData.delta.y * CosDegreeValue;
+        float zzof = eventData.delta.y * CosDegreeValue;
+
+
+        this.transform.Translate(new Vector3(xoffset+xxof, 0, zoffset+zzof) *Time.deltaTime, Space.Self);
+        Debug.LogError("position:" + this.transform.position);
+     //   curPosition.y = 1;
+        //this.transform.position = curPosition;
+    }
+
     public void OnPointerDown(PointerEventData eventData)
     {
-       // ViewControllerLocal.GetInstance().PressSpot(this.gameObject.name);
+        ViewControllerLocal.GetInstance().PressSpot(this.gameObject.name);
     }
 
     //监听抬起
     public void OnPointerUp(PointerEventData eventData)
     {
-       // ViewControllerLocal.GetInstance().PressSpot("");
+        ViewControllerLocal.GetInstance().PressSpot("");
     }
 
     //监听点击
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.LogError("OnPointerClick" + this.gameObject.name);
-      //  ViewControllerLocal.GetInstance().SetSelectSpot(this.gameObject.name);
+        ViewControllerLocal.GetInstance().SetSelectSpot(this.gameObject.name);
      //   this.gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
     }
 }
